2a5817f3cbd263d94755df79e9151aa094c8cfe8
Shane <shane8641@live.com>
2016-02-05 14:57:58 -0500

    simple bmi calculator functionality works. needs css and validation

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function calcBMI(){
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+    var total = (weight * 703) / (height * height);
+    var bmi = parseInt(total);
+    document.getElementById("result").value = bmi;
+}
+document.getElementById("submit").addEventListener("click", calcBMI, false);
 
